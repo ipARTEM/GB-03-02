0eb00edfd6fe0128684ade7006bd1883232181ef
Artem <a.khimin@yandex.ru>
2021-09-06 21:43:41 +0300

    Сделал AgentsController

---
+++ GB-03-02/Controllers/AgentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GB_03_02.Controllers
+{
+    public class AgentInfo
+    {
+        public int AgentId { get; }
+
+        public Uri AgentAddress { get; }
+    }
+}
+
+
+
--- GB-03-02/Startup.cs
+++ GB-03-02/Startup.cs
@@ -23,10 +23,8 @@ namespace GB_03_02
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,7 +32,6 @@ namespace GB_03_02
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
